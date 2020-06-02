fc7749582333b32a92c62606fd453c02fa2c600e
Zesheng Xing <44153625+zsxing99@users.noreply.github.com>
2020-06-02 01:20:28 -0700

    -home page finished

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,50 @@ import { Button } from "antd";
 import cover from "../assets/Shields-entrance_Credit-Hector-Villicana_1924x1282-960x600-c-center.jpg";
 import google from "../assets/google.jpg";
 import logo from "../assets/Logo.png";
+import {
+    BrowserRouter as Router,
+    Route
+} from "react-router-dom";
 
 class App extends React.Component {
   state = {
     loggedIn: false
   }
 
+  onClick = () => {
+    // TODO: google Oauth
+    console.log("hello")
+
+    // for test purpose set loggedIn true
+    this.setState({
+      loggedIn: true
+    })
+  }
+
   render() {
     return (
-        <div className="App">
+        <Router>
+          <div className="App">
             {this.state.loggedIn ?
-                <Main /> :
+                (
+                    <Main/>
+                ):
                 (
                     <div className="welcome-page">
                         <img id="welcome-img" src={cover} alt=""/>
                         <div className="login-page">
-                            <img id="logo-img" src={logo} alt=""/>
-                            <Button shape="round">
-                              <img id="login-img" src={google} alt=""/>
-                              Login with Google
-                            </Button>
+                          <img id="logo-img" src={logo} alt=""/>
+                          <Button shape="round" onClick={this.onClick}>
+                            <img id="login-img" src={google} alt=""/>
+                            Login with Google
+                          </Button>
                         </div>
                     </div>
                 )
             }
-        </div>
+          </div>
+
+        </Router>
     )
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,74 @@
 import React from 'react';
+import { Button } from "antd";
+import logo from "../assets/Logo.png";
+import "../styles/Main.css";
 
-class Main extends React.Component {
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    withRouter
+} from "react-router-dom";
 
-    render() {
-        return (
-            <header>
+function Main() {
+    const history = useHistory();
 
-            </header>
-        )
+    const onClickHomePage = () => {
+        history.push("/");
     }
+
+    return (
+        <div className="Main">
+            <header>
+                <div>
+                    <img src={logo} alt="" onClick={onClickHomePage}/>
+                </div>
+            </header>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Prompt} />
+                    <Route exact path="/finder" />
+                    <Route exact path="/seeker" />
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+function Prompt() {
+    const history = useHistory();
+
+    const onClickFinder = () => {
+        history.push("/finder")
+    }
+
+    const onClickSeeker = () => {
+        history.push("/seeker")
+    }
+
+    return (
+        <div className="prompt">
+            <div>
+                <h2 className="prompt-text">
+                    Did you find something?
+                </h2>
+                <Button onClick={onClickFinder} shape="round" id="finder-btn" type="text">
+                    I'm a finder
+                </Button>
+            </div>
+            <div>
+                <h2 className="prompt-text">
+                    Or are you looking for something?
+                </h2>
+                <Button onClick={onClickSeeker} shape="round" id="seeker-btn" type="text">
+                    I'm a seeker
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
